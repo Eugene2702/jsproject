f91f771b4737fd1319964c018a123df1e0b3c6c8
gleb <example@gmail.com>
2022-06-26 21:06:04 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,99 @@
+"use strict";
+
+//*******************ПЕРЕМЕННЫЕ И СТРОГИЙ РЕЖИМ******************** */
+//let number = 10;
+//const LeftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//const obj = {
+    //a: 50
+//};
+
+//obj.a = 10;
+//console.log(obj);
+
+
+//************ТИПЫ ДАННЫХ********************* */
+//let number1 = 4.6;
+
+//console.log(4/0);
+//console.log('string' * 9);
+
+//const persone = '5';
+//const bool = true;
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+
+//const object = {
+    //name: 'John',
+    //age: 25,
+    //isMarried: false
+//};
+
+//console.log(object.name);
+//console.log(object["name"]);
+
+
+
+
+
+//let arr = ['mamka.png', 'gleb.jpeg', 'apple.bmp', 6 ];
+//console.log(arr[1]);
+
+//********ПРОСТОЕ ОБЩЕНИЕ С ПОЛЬЗОВАТЕЛЕМ**************************/
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "");
+//console.log(typeof(answer));
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt("Как вас зовут?", "");
+//answers[1] = prompt("Какая у вас фамилия?", "");
+//answers[2] = prompt("Сколько вам лет?", "");
+
+//document.write(answers);
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+//************ИНТЕРПОЛЯЦИЯ**********************/
+
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = 'Gleb';
+//alert(`Привет, ${user}`);
+
+//**************ОПЕРАТОРЫ****************** */
+
+let incr = 10,
+    decr = 10;
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
+
+console.log(!isChecked || isClose);
+
+
+
+
